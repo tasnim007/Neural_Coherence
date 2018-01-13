48d692020092c7eb3c205c0d037d172c17f6e558
Dat Tien Nguyen <t.d.nguyen@uva.nl>
2018-01-13 23:05:18 +0100

    new keras by Dat

--- utilities/my_callbacks.py
+++ utilities/my_callbacks.py
@@ -14,20 +14,21 @@ class Histories(keras.callbacks.Callback):
 
 	def on_epoch_end(self, epoch, logs={}):
 		self.losses.append(logs.get('loss'))
-                y_pred = self.model.predict([self.validation_data[0],self.validation_data[1]])
-                #y_pred = self.model.predict([self.model.validation_data[0],self.model.validation_data[1]]) 
-                wins = 0
-                ties = 0
-                n = len(y_pred)
-                for i in range(0,n):
-                    if y_pred[i][0] > y_pred[i][1]:
-                        wins = wins + 1
-                    elif y_pred[i][0] == y_pred[i][1]:
-                        ties = ties + 1
-                print(" -Dev Wins: " + str(wins) + " Ties: "  + str(ties))
-                loss = n - (wins+ties)
-                prec = wins/(wins + loss)
-                print(" -Dev acc: " + str(wins/n))
+		y_pred = self.model.predict([self.validation_data[0],self.validation_data[1]])
+
+		#y_pred = self.model.predict([self.model.validation_data[0],self.model.validation_data[1]]) 
+		wins = 0
+		ties = 0
+		n = len(y_pred)
+		for i in range(0,n):
+			if y_pred[i][0] > y_pred[i][1]:
+				wins = wins + 1
+			elif y_pred[i][0] == y_pred[i][1]:
+				ties = ties + 1
+		print(" -Dev Wins: " + str(wins) + " Ties: "  + str(ties))
+		loss = n - (wins+ties)
+		prec = wins/(wins + loss)
+		print(" -Dev acc: " + str(wins/n))
 		self.accs.append(prec)
 
 		return
